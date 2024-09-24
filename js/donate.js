27d1a4d1631e2301c534e6ccf1517800bae813b0
Marina <marinaakter878@gmail.com>
2024-09-24 16:11:34 +0600

    interective-donation part done

--- js/donate.js
+++ js/donate.js
@@ -4,7 +4,6 @@ document.getElementById('btn-donate-1').addEventListener('click',function(event)
     const totalAmount = getTextFieldById('total-amount')
     const donatedAmountNoakhali = getInputFieldById('donation-amount')  
     const amountForNoakhali = getTextFieldById('amount-for-noakhali')
-    console.log(totalAmount, donatedAmountNoakhali, amountForNoakhali);
     
 
     if(isNaN(donatedAmountNoakhali)){
@@ -14,7 +13,7 @@ document.getElementById('btn-donate-1').addEventListener('click',function(event)
     
     const currentAmount = totalAmount - donatedAmountNoakhali
     const currentAmountForNoakhali = amountForNoakhali + donatedAmountNoakhali
-    console.log(currentAmount, currentAmountForNoakhali);
+
 
     if(currentAmount < 0){
         alert('You do not have enough money')
@@ -32,7 +31,7 @@ document.getElementById('btn-donate-2').addEventListener('click',function(event)
     const totalAmount = getTextFieldById('total-amount')
     const donatedAmountFeni = getInputFieldById('donated-amount-feni')  
     const amountForFeni = getTextFieldById('amount-for-feni')
-    console.log(donatedAmountFeni, amountForFeni);
+
     
 
     if(isNaN(donatedAmountFeni)){
@@ -42,7 +41,6 @@ document.getElementById('btn-donate-2').addEventListener('click',function(event)
     
     const currentAmountFeni = totalAmount - donatedAmountFeni
     const currentAmountForFeni = amountForFeni + donatedAmountFeni
-    console.log(currentAmountFeni, currentAmountForFeni);
 
     if(currentAmountFeni < 0){
         alert('You do not have enough money')
@@ -54,3 +52,33 @@ document.getElementById('btn-donate-2').addEventListener('click',function(event)
     
 })
 
+
+// Donation for Quota Movement
+document.getElementById('btn-donate-3').addEventListener('click',function(event){
+    event.preventDefault
+    const totalAmount = getTextFieldById('total-amount')
+    const donatedAmountQouta = getInputFieldById('donated-amount-qouta')  
+    const amountForQuota = getTextFieldById('amount-for-qouta')
+    
+
+    
+
+    if(isNaN(donatedAmountQouta)){
+        alert("Type Correct number to Donate money")
+        return
+    }
+    
+    const currentAmountQuota = totalAmount - donatedAmountQouta
+    const currentAmountForQuota = amountForQuota + donatedAmountQouta
+    
+
+
+    if(currentAmountQuota < 0){
+        alert('You do not have enough money')
+        return
+    }
+
+    document.getElementById('total-amount').innerText = `${currentAmountQuota} BDT`
+    document.getElementById('amount-for-qouta').innerText = `${currentAmountForQuota} BDT`
+    
+})
