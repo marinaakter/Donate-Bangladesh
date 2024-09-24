18f3bd7123df91207b5c4c50178bbd3d6c9b49e7
Marina <marinaakter878@gmail.com>
2024-09-24 15:47:17 +0600

    donation for noakhali & feni added

--- js/donate.js
+++ js/donate.js
@@ -1,17 +1,19 @@
-/* 
-document.getElementById('btn-donate').addEventListener('click',function(event){
+// Donation For Noakhali
+document.getElementById('btn-donate-1').addEventListener('click',function(event){
     event.preventDefault
     const totalAmount = getTextFieldById('total-amount')
-    const donationAmount = getInputFieldById('donation-amount')  
+    const donatedAmountNoakhali = getInputFieldById('donation-amount')  
     const amountForNoakhali = getTextFieldById('amount-for-noakhali')
+    console.log(totalAmount, donatedAmountNoakhali, amountForNoakhali);
+    
 
-    if(isNaN(donationAmount)){
+    if(isNaN(donatedAmountNoakhali)){
         alert("Type Correct number to Donate money")
         return
     }
     
-    const currentAmount = totalAmount - donationAmount
-    const currentAmountForNoakhali = amountForNoakhali + donationAmount
+    const currentAmount = totalAmount - donatedAmountNoakhali
+    const currentAmountForNoakhali = amountForNoakhali + donatedAmountNoakhali
     console.log(currentAmount, currentAmountForNoakhali);
 
     if(currentAmount < 0){
@@ -23,12 +25,32 @@ document.getElementById('btn-donate').addEventListener('click',function(event){
     document.getElementById('amount-for-noakhali').innerText = `${currentAmountForNoakhali} BDT`
     
 })
-*/
 
-function donate(){
+// Donation for Feni
+document.getElementById('btn-donate-2').addEventListener('click',function(event){
+    event.preventDefault
     const totalAmount = getTextFieldById('total-amount')
-    const donationAmount = getInputFieldById('donation-amount')  
-    const totalDonated = getTextFieldById('total-donated')
-    return totalAmount, donationAmount, totalDonated
+    const donatedAmountFeni = getInputFieldById('donated-amount-feni')  
+    const amountForFeni = getTextFieldById('amount-for-feni')
+    console.log(donatedAmountFeni, amountForFeni);
     
-}
+
+    if(isNaN(donatedAmountFeni)){
+        alert("Type Correct number to Donate money")
+        return
+    }
+    
+    const currentAmountFeni = totalAmount - donatedAmountFeni
+    const currentAmountForFeni = amountForFeni + donatedAmountFeni
+    console.log(currentAmountFeni, currentAmountForFeni);
+
+    if(currentAmountFeni < 0){
+        alert('You do not have enough money')
+        return
+    }
+
+    document.getElementById('total-amount').innerText = `${currentAmountFeni} BDT`
+    document.getElementById('amount-for-feni').innerText = `${currentAmountForFeni} BDT`
+    
+})
+
